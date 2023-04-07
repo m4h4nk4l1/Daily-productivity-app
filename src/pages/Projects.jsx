5a99755f748d115f1f66e21ab8b0ca693745b8e8
maha <m4h4nk4l1@gmail.com>
2023-04-07 20:27:30 +0530

    todo UI

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"; 
-import code from "../assets/code.png"
-import project1 from "../assets/project1.png"
-import javascriptproject from "../assets/javascript-projects.png"
+import { motion } from "framer-motion";
+import code from "../assets/code.png";
+import project1 from "../assets/project1.png";
+import javascriptproject from "../assets/javascript-projects.png";
 const Projects = () => {
   return (
     <motion.div
@@ -13,109 +13,137 @@ const Projects = () => {
       className="bg-gradient-to-b min-h-screen from-cyan-100 to-blue-400 text-xl font-shan"
     >
       <div className="mx-40 h-[100%]">
-        <section>
-          <h2 className="font-semibold text-4xl pt-8">Projects I'm proud of</h2>
-          <article class="pt-8 flex flex-row-reverse">
-            <div class="text">
-              <h4 className="text-2xl font-semibold">Crypto Explorer site</h4>
-              <p className="">
-                Description of the project. This should be fairly concise while
-                also describing the key components that you developed or worked
-                on. It can be as long as you need it to be but should at least
-                be a few sentences long. Be sure to include specific links
-                anywhere in the description. Live link to the project <a className="underline decoration-auto hover:no-underline" href="https://642d8bcddb12244c5445799f--cryto-explorer-m4h4nk4l1.netlify.app/">netlify</a>.
-                
-              </p>
-              <h4 className="font-semibold">Technologies used include:</h4>
-              <ul>
-                <li>ReactJS</li>
-                <li>Tailwind Css</li>
-                <li>Context-Api</li>
-              </ul>
-            </div>
-            <img
-              src={code}
-              width="700"
-              height="380"
-              alt="Screenshot of code."
-              className=" p-4  border-purple-600 rounded-2xl border-l-4"
-            />
-          </article>
-          
-          <article className="pt-8 flex flex-row">
-            <div >
-              <h3 className="text-2xl font-semibold">Construction Materials</h3>
-              <p class>
-                It is a online purchasing construction material ecommerce site
-                where you can purchase different construction materials of
-                different brands listed. It is a frontend website with
-                <a className="underline decoration-auto hover:no-underline" href="https://github.com/m4h4nk4l1/reactJS-construction">
-                   Link
-                </a>
-                .
-              </p>
-              <h4 className="font-semibold">Technologies used include:</h4>
-              <ul>
-                <li>reactJS</li>
-                <li>Bootstrap</li>
-                <li>Json-server</li>
-              </ul>
-            </div>
-            <img
-              src={project1}
-              width="700"
-              height="380"
-              alt="Screenshot of the Wall of Wonder."
-              className=" p-4  border-purple-600 rounded-2xl border-r-4"
-            />
-          </article>
+        <h2 className="font-semibold text-4xl pt-8">Projects I'm proud of</h2>
 
-          <article className="pt-8 flex flex-row-reverse">
-            <div >
-              <h3 className="text-2xl font-semibold">Multiple Javascript Features</h3>
-              <p class>
-                It is a online purchasing construction material ecommerce site
-                where you can purchase different construction materials of
-                different brands listed. It is a frontend website with
-                <a className="underline decoration-auto hover:no-underline" href="https://github.com/m4h4nk4l1/reactJS-construction">
-                   Link
-                </a>
-                .
-              </p>
-              <h4 className="font-semibold">Technologies used include:</h4>
-              <ul>
-                <li>Html</li>
-                <li>Css</li>
-                <li>Javascript</li>
-              </ul>
-            </div>
-            <img
-              src={javascriptproject}
-              width="700"
-              height="380"
-              alt="Screenshot of the Wall of Wonder."
-              className=" p-4  border-purple-600 rounded-2xl border-l-4"
-            />
-          </article>
-          
-          <div class="flex flex-col h-screen bg-center bg-cover ">
-			<div class="grid place-items-center w-4/5 mx-auto p-10 my-20 sm:my-auto bg-white-600 border-4 border-black bg-opacity-70 rounded-xl shadow-2xl space-y-5 text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-24 w-24 text-indigo-600" viewBox="0 0 16 16">
-  <path d="M4.968 9.75a.5.5 0 1 0-.866.5A4.498 4.498 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.498 3.498 0 0 1 8 11.5a3.498 3.498 0 0 1-3.032-1.75zM7 5.116V5a1 1 0 0 0-1-1H3.28a1 1 0 0 0-.97 1.243l.311 1.242A2 2 0 0 0 4.561 8H5a2 2 0 0 0 1.994-1.839A2.99 2.99 0 0 1 8 6c.393 0 .74.064 1.006.161A2 2 0 0 0 11 8h.438a2 2 0 0 0 1.94-1.515l.311-1.242A1 1 0 0 0 12.72 4H10a1 1 0 0 0-1 1v.116A4.22 4.22 0 0 0 8 5c-.35 0-.69.04-1 .116z"/>
-  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-1 0A7 7 0 1 0 1 8a7 7 0 0 0 14 0z"/>
-</svg>
-        
-				<h1 class="text-4xl font-bold uppercase text-black transition duration-500">Coming Soon</h1>
-        <h2 class="text-xl text-black font-semibold"> I Building more. Checkout later.......</h2>
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-          
-           
-          
-        </div>
-			</div>
-		</div>
+        {/* project 1 */}
 
+        <section class="pt-8 flex flex-row-reverse">
+          <div class="text">
+            <h4 className="text-2xl font-semibold">Crypto Explorer site</h4>
+            <p className="">
+              Description of the project. This should be fairly concise while
+              also describing the key components that you developed or worked
+              on. It can be as long as you need it to be but should at least be
+              a few sentences long. Be sure to include specific links anywhere
+              in the description. Live link to the project{" "}
+              <a
+                className="underline decoration-auto hover:no-underline"
+                href="https://642d8bcddb12244c5445799f--cryto-explorer-m4h4nk4l1.netlify.app/"
+              >
+                netlify
+              </a>
+              .
+            </p>
+            <h4 className="font-semibold">Technologies used include:</h4>
+            <ul>
+              <li>ReactJS</li>
+              <li>Tailwind Css</li>
+              <li>Context-Api</li>
+            </ul>
+          </div>
+          <img
+            src={code}
+            width="700"
+            height="380"
+            alt="Screenshot of code."
+            className=" p-4  border-purple-600 rounded-2xl border-l-4"
+          />
         </section>
+
+        {/* project 2 */}
+
+        <section className="pt-8 flex flex-row">
+          <div>
+            <h3 className="text-2xl font-semibold">Construction Materials</h3>
+            <p class>
+              It is a online purchasing construction material ecommerce site
+              where you can purchase different construction materials of
+              different brands listed. It is a frontend website with
+              <a
+                className="underline decoration-auto hover:no-underline"
+                href="https://github.com/m4h4nk4l1/reactJS-construction"
+              >
+                Link
+              </a>
+              .
+            </p>
+            <h4 className="font-semibold">Technologies used include:</h4>
+            <ul>
+              <li>reactJS</li>
+              <li>Bootstrap</li>
+              <li>Json-server</li>
+            </ul>
+          </div>
+          <img
+            src={project1}
+            width="700"
+            height="380"
+            alt="Screenshot of the Wall of Wonder."
+            className=" p-4  border-purple-600 rounded-2xl border-r-4"
+          />
+        </section>
+
+        {/* project 3 */}
+
+        <section className="pt-8 flex flex-row-reverse">
+          <div>
+            <h3 className="text-2xl font-semibold">
+              Multiple Javascript Features
+            </h3>
+            <p class>
+              It is a online purchasing construction material ecommerce site
+              where you can purchase different construction materials of
+              different brands listed. It is a frontend website with
+              <a
+                className="underline decoration-auto hover:no-underline"
+                href="https://github.com/m4h4nk4l1/reactJS-construction"
+              >
+                Link
+              </a>
+              .
+            </p>
+            <h4 className="font-semibold">Technologies used include:</h4>
+            <ul>
+              <li>Html</li>
+              <li>Css</li>
+              <li>Javascript</li>
+            </ul>
+          </div>
+          <img
+            src={javascriptproject}
+            width="700"
+            height="380"
+            alt="Screenshot of the Wall of Wonder."
+            className=" p-4  border-purple-600 rounded-2xl border-l-4"
+          />
+        </section>
+
+        {/* coming soon section with logo */}
+
+        <div class="flex flex-col h-screen bg-center bg-cover ">
+          <div class="grid place-items-center w-4/5 mx-auto p-10 my-20 sm:my-auto bg-white-600 border-4 border-black bg-opacity-70 rounded-xl shadow-2xl space-y-5 text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="h-24 w-24 text-indigo-600"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.968 9.75a.5.5 0 1 0-.866.5A4.498 4.498 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.498 3.498 0 0 1 8 11.5a3.498 3.498 0 0 1-3.032-1.75zM7 5.116V5a1 1 0 0 0-1-1H3.28a1 1 0 0 0-.97 1.243l.311 1.242A2 2 0 0 0 4.561 8H5a2 2 0 0 0 1.994-1.839A2.99 2.99 0 0 1 8 6c.393 0 .74.064 1.006.161A2 2 0 0 0 11 8h.438a2 2 0 0 0 1.94-1.515l.311-1.242A1 1 0 0 0 12.72 4H10a1 1 0 0 0-1 1v.116A4.22 4.22 0 0 0 8 5c-.35 0-.69.04-1 .116z" />
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-1 0A7 7 0 1 0 1 8a7 7 0 0 0 14 0z" />
+            </svg>
+
+            <h1 class="text-4xl font-bold uppercase text-black transition duration-500">
+              Coming Soon
+            </h1>
+            <h2 class="text-xl text-black font-semibold">
+              {" "}
+              I Building more. Checkout later.......
+            </h2>
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-4"></div>
+          </div>
+        </div>
       </div>
 
       <Link to="/projects"></Link>
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,26 +1,91 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {motion} from 'framer-motion';
-import todo from '../assets/todo.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import todo from "../assets/todo.jpg";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { TbTrash } from "react-icons/tb";
 
 const Todo = () => {
+
+    
+
+
   return (
     <motion.div
-    initial={{ height: 0 }}
-      animate={{height: "100%"}}
-      exit={{ y: window.innerHeight, trasition: {duration: 0.2}}}
+      initial={{ height: 0 }}
+      animate={{ height: "100%" }}
+      exit={{ y: window.innerHeight, trasition: { duration: 0.2 } }}
       className="bg-gradient-to-b min-h-screen from-cyan-100 to-blue-400 bg-cover"
     >
-      <div className='border '>
-        <section className='flex flex-initial flex-row items-center justify-center'>
-          <h2> TODO </h2>
-          <img className='w-20' src={todo} alt="todo-icon" />
+
+      {/* header and logo section call it header component */}
+
+      <section className="container  border-blue-400 w-[80%] h-[100%] border-2 mx-auto my-auto shadow-black shadow-xl ">
+        <header className="flex flex-wrap my-6 items-center space-x-0 justify-center">
+          <img className="w-20 h-auto" src={todo} alt="todo-icon" />
+          <h2 className="font-shan text-2xl"> TODO </h2>
+        </header>
+        <form className="flex flex-row justify-center mb-6 ">
+          <input
+            type="text"
+            className=" w-[60%] mr-4 text-start placeholder: pl-2 bg-blue-300 placeholder-slate-700 text-black font-shan rounded-sm"
+            placeholder="Add a New Task"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold font-shan py-2 px-4 rounded flex align-middle items-center"
+            type="submit"
+          >
+            Create <AiOutlinePlusCircle className="ml-1" size={20} />
+          </button>
+        </form>
+      </section>
+
+      {/* so this section is after the above section which has border and shadow  */}
+
+      <section>
+      
+        {/* this section is header section where you can see the count of tasks */}
+
+        <header className="flex flex-row justify-around content-around">
+          <div className="flex flex-row space-x-2 mt-8">
+            <p className="text-blue-900 text-lg font-shan font-semibold">
+              Created Tasks
+            </p>
+            <span className="border rounded-full bg-blue-300 text-black px-1 border-slate-600">
+              {" "}
+              0{" "}
+            </span>
+          </div>
+          <div className="flex flex-row space-x-2 mt-8">
+            <p className="text-violet-900 text-lg font-shan font-semibold">
+              Completed Tasks
+            </p>
+            <span className="border rounded-full bg-blue-300 text-black px-1 border-slate-500">
+              {" "}
+              0 of 10{" "}
+            </span>
+          </div>
+        </header>
+
+        {/* this section is for the tasks that we get we call task */}
+
+        <section className="flex flex-col ">
+          <div className="flex flex-row m-auto top-4 pl-5 bg-blue-300 font-shan space-y-4 overflow-auto border rounded-md border-slate-700 my-8 w-[60%]">
+            <button>
+              <BsFillCheckCircleFill />
+            </button>
+            <p className="items-center ">hey there this is the task Created</p>
+            <button>
+              <TbTrash className="content-end " size={20} />
+            </button>
+          </div>
         </section>
-      </div>
+      </section>
+
       <Link to="/todo"></Link>
-      </motion.div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default Todo
-
+export default Todo;
