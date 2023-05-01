e99fd0f5e4f2d7a019dc819e9aeda7b8c27ea2d9
maha <m4h4nk4l1@gmail.com>
2023-05-01 16:11:07 +0530

    Ui updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,14 @@ const Home = () => {
       className="bg-gradient-to-b min-h-screen font-shan from-cyan-100 to-blue-400 bg-cover text-2xl"
     >
       {/* <Link to="/"></Link> */}
-      <div className="ml-40 pt-32 max-w-[75%] h-full text-lg" id="intro">
+      <div className="text-sm flex flex-row space-x-2 items-center justify-end mr-20 pt-4">
+        <button className="border bg-gradient-to-t from-cyan-100 to-blue-400 border-spacing-2 border-current p-1 hover:border-cyan-600 rounded-md">
+          My Resume
+        </button>
+      </div>
+      <div className="ml-40 pt-24 max-w-[75%] h-full text-lg" id="intro">
         <p className="mb-8">Hi, my name is</p>
-        <span className="pt-14 text-5xl text-black font-semibold">
+        <span className="pt-14 text-5xl text-black font-semibold ">
           Mahankali Phani Manohar Sai.
         </span>
         <h2 className="font-bold text-3xl pt-10">I'm a software developer</h2>
@@ -21,8 +26,8 @@ const Home = () => {
           Proficient in ReactJS, Tailwindcss, Redux and three building blocks of
           the web. <br />
           <br />
-          Currently learning Typescript, nextJS and Focusing on Backend to
-          become Fullstack Enginner. <br />
+          Currently learning Typescript, Nextjs and focusing on Backend to
+          become Fullstack Engineer. <br />
           <br />
           My work in code{" "}
           <a
@@ -33,6 +38,9 @@ const Home = () => {
           </a>
         </p>
       </div>
+      <p className="text-sm mt-32 ml-40 flex justify-start ">
+        chill out while checking my portfolio scroll through the sidebar
+      </p>
     </motion.div>
   );
 };
