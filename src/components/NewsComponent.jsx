8b99c3707717ea0693448b8f04b77bce85f6cdd9
maha <m4h4nk4l1@gmail.com>
2023-05-18 22:03:19 +0530

    updates

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import tasks from "../assets/edit.png";
 
 const NewsComponent = () => {
   return (
     <div className="border flex border-transparent rounded-lg shadow-slate-500 shadow-lg p-2">
-      <h4>Hindustan Times</h4>
-      <p className="w-56 pt-2">
-        Description of the news that comes from the NewsAPI and for eg: we get
-        this long beautiful dreams and to achieve that we need to plan and take
-        action seroiusly
-      </p>
-      <img className="" src="../assets/edit.png" alt="code.png" />
+      <div className="flex flex-1 pl-2 flex-col">
+        <h4>Hindustan Times</h4>
+        <p className="w-[3/5] flex-1 pt-2">
+          Description of the news that comes from the NewsAPI and for eg: we get
+          this long beautiful dreams and to achieve that we need to plan and
+          take action seroiusly
+        </p>
+      </div>
+      <img className=" pl-4 pt-2 w-[30%] h-[80%]" src={tasks} alt="code.png" />
     </div>
   );
 };
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import NewsComponent from "../components/NewsComponent";
 
 const Search = () => {
-  let req = new Request(
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=${
-      import.meta.env.VITE_API_KEY
-    }`
-  );
-  fetch(req).then(function (response) {
-    console.log(response.json());
-  });
+  async function requestNews() {
+    const res = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    const json = await res.json();
+  }
+  console.log(json);
+
+  // const [sourceName, setSourceName] = useState();
+  // const [description, setDescription] = useState();
+  // const [image, setImage] = useState();
 
   return (
     <motion.div
       initial={{ height: 0 }}
       animate={{ height: "100%" }}
       exit={{ y: window.innerHeight, transition: { duration: 0.2 } }}
-      className="bg-gradient-to-b min-h-[100vh] flex justify-center items-center from-cyan-100 to-blue-400 bg-cover font-shan"
+      className="bg-gradient-to-b min-h-screen flex justify-center items-center from-cyan-100 to-blue-400 bg-cover font-shan"
     >
       <div className="w-[80%] mt-8 ">
         {/* search bar component */}
@@ -33,9 +38,9 @@ const Search = () => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </div>
@@ -58,16 +63,17 @@ const Search = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
           </button>
         </form>
-        <section className="mt-8 border border-transparent h-[80%]  rounded-lg text-justify">
-          <div className="grid grid-cols-3 gap-6 ">
+        {/* latest headlines */}
+        <section className="mt-8 border border-transparent mb-8 rounded-lg text-justify">
+          <div className="grid grid-cols-3 gap-6">
             <NewsComponent />
             <NewsComponent />
             <NewsComponent />
